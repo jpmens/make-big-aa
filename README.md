5e781f00383eb228eb7b6a8364396a07a537f972
Jan-Piet Mens <jpmens@gmail.com>
2011-02-02 22:05:47 +0100

    start on rewrite

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 this makes big aa ;-)
 **don't** use it.
+
+## Credits
+
+* Fortune cookies in TXT RR are from the [http://wertarbyte.de/gigaset-rss/](Gigaset Fortune-RSS generator)
+* [http://pages.cs.wisc.edu/~ballard/bofh/excuses](BOFH excuses)
+
--- makebig.pl
+++ makebig.pl
@@ -5,20 +5,26 @@
 # 2010-02-02 continues being dirty; added NS glue and CNAMEs
 
 use strict;
+use File::Slurp qw(slurp);
+use PerlIO::gzip;
 
-my $words = '/usr/share/dict/words';
 my $ipv6 = '2001:0db8:85a3:0000:0000:8a2e:0370:7334';
 
-my @wl = slurpwords($words);
+
+
+my @wl = slurpwords('wordlist.gz'); 	# words MUST be unique
 my @cnames = (qw(google.com yahoo.de cnn.com powerdns.org isc.org nlnetlabs.nl));
 my $n = 0;
 my %unique;
 
+my @fortunes = slurp('fortunes');
+chomp(@fortunes);
+
 srand (time ^ $$ ^ unpack "%L*", `ps axww | gzip -f`);
 
-soa();
+print slurp('incl/SOA.in'), "\n";
+print slurp('incl/delegations.in'), "\n";
 
-glue();
 
 while ($n < 1000000) {
 
@@ -43,7 +49,7 @@ while ($n < 1000000) {
 	if (!($n % 5)) {
 		printf "%-40s IN MX  10 xs.powerdns.com.\n", "";
 		printf "%-40s IN AAAA  2001:1178:6bf::422:9871:1034:1fdb\n", "";
-		printf "%-40s IN TXT \"%s\"\n", "", fortune();
+		printf "%-40s IN TXT \"%s\"\n", "", @fortunes[ int(rand($#fortunes)) ];
 	}
 
 	if (!($n % 9)) {
@@ -65,48 +71,34 @@ while ($n < 1000000) {
 
 print "jp-was-here IN TXT \"end-of-file\"\n";
 
-sub soa {
-	print <<END;
-\$TTL 600        ; 10 minutes
-@                       IN SOA  ns.big.aa. hostmaster.big.aa. (
-                                3         ; serial
-                                7200       ; refresh (2 hours)
-                                300        ; retry (5 minutes)
-                                10800      ; expire (3 hours)
-                                600        ; minimum (10 minutes)
-                                )
-                        NS      localhost.big.aa.
-localhost.big.aa.                       A       127.0.0.1
-END
-}
-
 sub slurpwords {
 	my ($file) = @_;
-	my @list;
-	my $w;
+	my @words;
 
-	# add more words by pushing in -0 -1, ...
-	open(WORDS, $file) or die "Can't open words at $file: $!\n";
+	open WORDS, "<:gzip", $file or die $!;
 	while (<WORDS>) {
 		next unless (/^[a-z-]+$/i);
+		next if (/^-/);
 		chomp;
-		$w = lc;
-		push(@list, $w);
-		push(@list, "$w-0");
-		push(@list, "$w-1");
-		push(@list, "$w-2");
-		push(@list, "$w-3");
-		push(@list, "$w-aa");
-		push(@list, "$w-ab");
-		push(@list, "$w-jp");
-		push(@list, "$w-ah");
-		push(@list, "$w-44");
-		push(@list, "$w-54");
-
+		
+		my $w = lc $_;
+		# add more words by pushing in -0 -1, ...
+		push(@words, "${w}");
+		push(@words, "${w}-0");
+		push(@words, "${w}-1");
+		push(@words, "${w}-2");
+		push(@words, "${w}-3");
+		push(@words, "${w}-aa");
+		push(@words, "${w}-ab");
+		push(@words, "${w}-jp");
+		push(@words, "${w}-ah");
+		push(@words, "${w}-44");
+		push(@words, "${w}-54");
+		push(@words, "${w}-bh");
+		push(@words, "${w}-six");
 	}
-	close(WORDS);
-
-	return @list;
+	close WORDS;
+	return @words;
 }
 
 sub printkeys {
@@ -125,188 +117,3 @@ sub printcert()
 END
 }
 
-sub fortune {
-	# http://fortunes.pbworks.com/w/page/14107107/computers
-	my @list = (
-'A formal parsing algorithm should not always be used.',
-'A Fortran compiler is the hobgoblin of little minis.',
-'A hacker does for love what others would not do for money.',
-'A language that doesnt affect the way you think about programming is not worth knowing.',
-'A rolling disk gathers no MOS.',
-'A sheet of paper crossed my desk the other day and as I read it',
-'JCL support as alternative to system menu.',
-	);
-
-	return $list[ int(rand($#list)) ];
-}
-
-sub glue {
-	print <<ENDGLUE;
-rootae.ns			IN 	A	198.41.0.4
-rootaf.ns			IN 	A	198.41.0.4
-rootar.ns			IN 	AAAA	2001:503:ba3e::2:30
-rootbe.ns			IN 	A	192.228.79.201
-rootbg.ns			IN 	A	192.33.4.12
-rootbj.ns			IN 	A	128.8.10.90
-rootca.ns			IN 	A	192.203.230.10
-rootch.ns			IN 	A	192.5.5.241
-rootcl.ns			IN 	AAAA	2001:500:2f::f
-rootcn.ns			IN 	A	192.112.36.4
-rootco.ns			IN 	A	128.63.2.53
-rootcr.ns			IN 	A	198.41.0.4
-rootde.ns			IN 	A	198.41.0.4
-rootdj.ns			IN 	A	198.41.0.4
-rootdk.ns			IN 	AAAA	2001:503:ba3e::2:30
-rootdo.ns			IN 	A	192.228.79.201
-rootee.ns			IN 	A	192.33.4.12
-rootes.ns			IN 	A	128.8.10.90
-rootet.ns			IN 	A	192.203.230.10
-rooteu.ns			IN 	A	192.5.5.241
-rootfi.ns			IN 	AAAA	2001:500:2f::f
-rootfr.ns			IN 	A	192.112.36.4
-rootgr.ns			IN 	A	128.63.2.53
-roothu.ns			IN 	A	192.36.148.17
-rootid.ns			IN 	A	192.58.128.30
-rootie.ns			IN 	A	193.0.14.129
-rootil.ns			IN 	AAAA	2001:7fd::1
-rootin.ns			IN 	A	199.7.83.42
-rootiq.ns			IN 	AAAA	2001:503:ba3e::2:30
-rootis.ns			IN 	A	192.228.79.201
-rootit.ns			IN 	A	192.33.4.12
-rootjp.ns			IN 	A	128.8.10.90
-rootkr.ns			IN 	A	192.203.230.10
-rootkw.ns			IN 	A	192.5.5.241
-rootla.ns			IN 	AAAA	2001:500:2f::f
-rootlb.ns			IN 	A	192.112.36.4
-rootlt.ns			IN 	A	128.63.2.53
-rootlu.ns			IN 	A	192.36.148.17
-rootlv.ns			IN 	A	192.58.128.30
-rootly.ns			IN 	A	193.0.14.129
-rootmg.ns			IN 	AAAA	2001:7fd::1
-rootmx.ns			IN 	A	199.7.83.42
-rootmy.ns			IN 	AAAA	2001:503:ba3e::2:30
-rootne.ns			IN 	A	192.228.79.201
-rootng.ns			IN 	A	192.33.4.12
-rootni.ns			IN 	A	128.8.10.90
-rootnl.ns			IN 	A	192.203.230.10
-rootno.ns			IN 	A	192.5.5.241
-rootpa.ns			IN 	AAAA	2001:500:2f::f
-rootpe.ns			IN 	A	192.112.36.4
-rootph.ns			IN 	A	128.63.2.53
-rootpk.ns			IN 	A	192.36.148.17
-rootpl.ns			IN 	A	192.58.128.30
-rootpr.ns			IN 	A	193.0.14.129
-rootps.ns			IN 	AAAA	2001:7fd::1
-rootqa.ns			IN 	A	199.7.83.42
-rootro.ns			IN 	A	192.36.148.17
-rootru.ns			IN 	A	192.58.128.30
-rootrw.ns			IN 	A	193.0.14.129
-rootsa.ns			IN 	AAAA	2001:7fd::1
-rootsd.ns			IN 	A	199.7.83.42
-rootse.ns			IN 	AAAA	2001:503:ba3e::2:30
-rootsg.ns			IN 	A	192.228.79.201
-rootsl.ns			IN 	A	192.33.4.12
-rootsn.ns			IN 	A	128.8.10.90
-rootso.ns			IN 	A	192.203.230.10
-rootsr.ns			IN 	A	192.5.5.241
-roottd.ns			IN 	AAAA	2001:500:2f::f
-rootth.ns			IN 	A	192.112.36.4
-roottl.ns			IN 	A	128.63.2.53
-roottn.ns			IN 	A	192.36.148.17
-roottr.ns			IN 	A	192.58.128.30
-roottw.ns			IN 	A	193.0.14.129
-rootua.ns			IN 	AAAA	2001:7fd::1
-rootug.ns			IN 	A	199.7.83.42
-rootuk.ns			IN 	A	198.41.0.4
-rootun.ns			IN 	AAAA	2001:503:ba3e::2:30
-rootus.ns			IN 	A	192.228.79.201
-rootuy.ns			IN 	A	192.33.4.12
-rootve.ns			IN 	A	128.8.10.90
-rootvn.ns			IN 	A	192.203.230.10
-rootws.ns			IN 	A	192.5.5.241
-rootye.ns			IN 	AAAA	2001:500:2f::f
-ae.ns			IN	NS	rootae.ns
-af.ns			IN	NS	rootaf.ns
-ar.ns			IN	NS	rootar.ns
-be.ns			IN	NS	rootbe.ns
-bg.ns			IN	NS	rootbg.ns
-bj.ns			IN	NS	rootbj.ns
-ca.ns			IN	NS	rootca.ns
-ch.ns			IN	NS	rootch.ns
-cl.ns			IN	NS	rootcl.ns
-cn.ns			IN	NS	rootcn.ns
-co.ns			IN	NS	rootco.ns
-cr.ns			IN	NS	rootcr.ns
-de.ns			IN	NS	rootde.ns
-dj.ns			IN	NS	rootdj.ns
-dk.ns			IN	NS	rootdk.ns
-do.ns			IN	NS	rootdo.ns
-ee.ns			IN	NS	rootee.ns
-es.ns			IN	NS	rootes.ns
-et.ns			IN	NS	rootet.ns
-eu.ns			IN	NS	rooteu.ns
-fi.ns			IN	NS	rootfi.ns
-fr.ns			IN	NS	rootfr.ns
-gr.ns			IN	NS	rootgr.ns
-hu.ns			IN	NS	roothu.ns
-id.ns			IN	NS	rootid.ns
-ie.ns			IN	NS	rootie.ns
-il.ns			IN	NS	rootil.ns
-in.ns			IN	NS	rootin.ns
-iq.ns			IN	NS	rootiq.ns
-is.ns			IN	NS	rootis.ns
-it.ns			IN	NS	rootit.ns
-jp.ns			IN	NS	rootjp.ns
-kr.ns			IN	NS	rootkr.ns
-kw.ns			IN	NS	rootkw.ns
-la.ns			IN	NS	rootla.ns
-lb.ns			IN	NS	rootlb.ns
-lt.ns			IN	NS	rootlt.ns
-lu.ns			IN	NS	rootlu.ns
-lv.ns			IN	NS	rootlv.ns
-ly.ns			IN	NS	rootly.ns
-mg.ns			IN	NS	rootmg.ns
-mx.ns			IN	NS	rootmx.ns
-my.ns			IN	NS	rootmy.ns
-ne.ns			IN	NS	rootne.ns
-ng.ns			IN	NS	rootng.ns
-ni.ns			IN	NS	rootni.ns
-nl.ns			IN	NS	rootnl.ns
-no.ns			IN	NS	rootno.ns
-pa.ns			IN	NS	rootpa.ns
-pe.ns			IN	NS	rootpe.ns
-ph.ns			IN	NS	rootph.ns
-pk.ns			IN	NS	rootpk.ns
-pl.ns			IN	NS	rootpl.ns
-pr.ns			IN	NS	rootpr.ns
-ps.ns			IN	NS	rootps.ns
-qa.ns			IN	NS	rootqa.ns
-ro.ns			IN	NS	rootro.ns
-ru.ns			IN	NS	rootru.ns
-rw.ns			IN	NS	rootrw.ns
-sa.ns			IN	NS	rootsa.ns
-sd.ns			IN	NS	rootsd.ns
-se.ns			IN	NS	rootse.ns
-sg.ns			IN	NS	rootsg.ns
-sl.ns			IN	NS	rootsl.ns
-sn.ns			IN	NS	rootsn.ns
-so.ns			IN	NS	rootso.ns
-sr.ns			IN	NS	rootsr.ns
-td.ns			IN	NS	roottd.ns
-th.ns			IN	NS	rootth.ns
-tl.ns			IN	NS	roottl.ns
-tn.ns			IN	NS	roottn.ns
-tr.ns			IN	NS	roottr.ns
-tw.ns			IN	NS	roottw.ns
-ua.ns			IN	NS	rootua.ns
-ug.ns			IN	NS	rootug.ns
-uk.ns			IN	NS	rootuk.ns
-un.ns			IN	NS	rootun.ns
-us.ns			IN	NS	rootus.ns
-uy.ns			IN	NS	rootuy.ns
-ve.ns			IN	NS	rootve.ns
-vn.ns			IN	NS	rootvn.ns
-ws.ns			IN	NS	rootws.ns
-ye.ns			IN	NS	rootye.ns
-ENDGLUE
-}
